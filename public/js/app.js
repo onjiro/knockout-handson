5f88615f109eee95373fa6c3964b7681bdf3116d
onjiro <onjiro.mohyahya@gmail.com>
2013-04-29 23:27:05 +0900

    Introduction Todo model

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
-// application code to be written
+(function() {
+    /**
+     * ひとつ分のTodo項目をに相当するモデルのコンストラクタ
+     */
+    var Todo = function(title, completed) {
+        this.title = ko.observable(title);
+        this.completed = ko.observable(completed);
+        this.editing = ko.observable(editing);
+    }
+
+})();
